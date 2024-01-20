909664a5d41cfdfb92c748bcd3d417c862f7ac33
carbontech-nwogu-michael-daberechi <michael@carbontechengineering.ng>
2024-01-20 17:40:58 +0100

    Stopping cos of issues wirh rapid api

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # MD-LANG-CONVERTER
+
+## Idea
+
+Implement an api that accept a file of texts of any language and converts it into a different one preferrable english.
+
+process
+// express-file-upload to receive incoming md file request
+// fs module to save the file in a public folder,access the data in the files, create files for the translate files
+// google translations for interpreting data
+// fs to return either all the response in text or in an md document downloadable format or as a link
+// tests for endpoint created
+// readme doc for what's being done or what we're about to do
--- server.js
+++ server.js
@@ -20,80 +20,8 @@ app.use(
   })
 );
 
-
-app.post("/detect", async (req, res) => {
-  const encodedParams = new URLSearchParams();
-  encodedParams.set("q", "English is hard, but detectably so");
-
-  const options = {
-    method: "POST",
-    url: process.env.DETECT_URL,
-    headers: {
-      "content-type": "application/x-www-form-urlencoded",
-      "Accept-Encoding": "application/gzip",
-      "X-RapidAPI-Key": process.env.RAPID_API_KEY,
-      "X-RapidAPI-Host": process.env.HOST_URL,
-    },
-    data: encodedParams,
-  };
-
-  try {
-    const response = await axios.request(options);
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-app.get("/languages", async (req, res) => {
-  const options = {
-    method: "GET",
-    url: process.env.LANGUAGES_URL,
-    headers: {
-      "Accept-Encoding": "application/gzip",
-      "X-RapidAPI-Key": process.env.RAPID_API_KEY,
-      "X-RapidAPI-Host": process.env.HOST_URL,
-    },
-  };
-
-  try {
-    const response = await axios.request(options);
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
-})
-
-
-app.post("/upload", async (req, res) => {
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).json({ msg: "No files were uploaded." });
-  }
-  const file = await req.files.file;
-
-    // Validate file type
-  if (file.mimetype !== 'text/markdown') {
-    return res.status(415).send('Unsupported Media Type. Only Markdown files are allowed.');
-  }
-
-//   // Move the file to a designated location (e.g., uploads directory)
-//   const uploadPath = __dirname + '/uploads/' + file.name;
-
-//   fileile.mv(uploadPath, (err) => {
-//     if (err) {
-//       return res.status(500).send(err);
-//     }
-// // res.send("File uploaded!");
-    
-//   });
-
-// return
-  const uploadedFile = req.files.file;
-  const from = await req.body.from;
-  const to = await req.body.to;
-
-
-  // Read the contents of the uploaded file
+app.post("/inspect", async (req, res) => {
+const uploadedFile = req.files.file;
   fs.readFile(uploadedFile.tempFilePath, "utf8", (err, data) => {
     if (err) {
       console.error(err);
@@ -101,18 +29,15 @@ app.post("/upload", async (req, res) => {
         .status(500)
         .json({ error: "An error occurred while reading the file" });
     }
-
     const fileContents = data.split("\n").join(" ");
-
-    // ------------------translate api --------------
+ 
+ 
     const encodedParams = new URLSearchParams();
     encodedParams.set("q", fileContents);
-    encodedParams.set("target", to);
-    encodedParams.set("source", from);
-
+  
     const options = {
       method: "POST",
-      url: process.env.TRANSLATION_URL,
+      url: process.env.DETECT_URL,
       headers: {
         "content-type": "application/x-www-form-urlencoded",
         "Accept-Encoding": "application/gzip",
@@ -121,44 +46,95 @@ app.post("/upload", async (req, res) => {
       },
       data: encodedParams,
     };
-
+  
     try {
       const response = axios.request(options);
       console.log(response.data);
-
-          return res.status(200).json({
-            successful: true,
-            message: "File uploaded and processed successfully",
-            content: fileContents,
-          });
+      res.json({
+        successful: true,
+        data:response.data
+      })
     } catch (error) {
       console.error(error);
     }
-    // ------------------ end translate api -------
-
-    // -----------downloadable file----------
-
-     const filePath = "/path/to/your/file.txt"; // Replace with the actual path to your file
-
-     // Set headers to specify the file name and type
-     res.setHeader(
-       "Content-Disposition",
-       'attachment; filename="downloaded-file.txt"'
-     );
-     res.setHeader("Content-Type", "text/plain"); // Adjust the content type based on your file type
-
-     // Send the file
-     res.sendFile(filePath, (err) => {
-       if (err) {
-         console.error(err);
-         res.status(500).send("Internal Server Error");
-       }
-     });
-    
-    // ---------------end downloadble file ----------------
-
-
   });
+});
+
+
+app.post("/upload", async (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).json({ msg: "No files were uploaded." });
+  }
+  const file = req.files.file;
+
+  // Validate file type
+  if (file.mimetype !== "text/markdown") {
+    return res
+      .status(415)
+      .send("Unsupported Media Type. Only Markdown files are allowed.");
+  }
+
+  //   // Move the file to a designated location (e.g., uploads directory)
+  //   const uploadPath = __dirname + '/uploads/' + file.name;
+
+  //   fileile.mv(uploadPath, (err) => {
+  //     if (err) {
+  //       return res.status(500).send(err);
+  //     }
+  // // res.send("File uploaded!");
+
+  //   });
+  // return
+
+  const uploadedFile = req.files.file;
+  const from = await req.body.from;
+  const to = await req.body.to;
+
+  // Read the contents of the uploaded file
+  
+    fs.readFile(uploadedFile.tempFilePath, "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return res
+          .status(500)
+          .json({ error: "An error occurred while reading the file" });
+      }
+      const fileContents = data.split("\n").join(" ");
+ 
+
+      // ------------------translate api --------------
+      const encodedParams = new URLSearchParams();
+      encodedParams.set("q", fileContents);
+      encodedParams.set("target", to);
+      encodedParams.set("source", from);
+
+      const options = {
+        method: "POST",
+        url: process.env.TRANSLATION_URL,
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+          "Accept-Encoding": "application/gzip",
+          "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+          "X-RapidAPI-Host": process.env.HOST_URL,
+        },
+        data: encodedParams,
+      };
+
+      try {
+        const response = axios.request(options);
+        console.log(response.data);
+
+        return res.status(200).json({
+          successful: true,
+          message: "File",
+          content: response.data,
+        });
+      } catch (error) {
+        console.error(error);
+      }
+      // ------------------ end translate api -------
+    })
+   
 });
 
 const port = process.env.PORT || 3000;
